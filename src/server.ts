87986424bfdb364d1ce612668f777d34f824c588
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-06 11:26:05 -0800

    [ADD] Mongoose database connection added.

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,18 @@
+import mongoose from 'mongoose';
+
 import app from './app';
 
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
-});
+(async function () {
+  mongoose.set({ strictQuery: true });
+  mongoose
+    .connect('mongodb://127.0.0.1:27017/news-app-db')
+    .then(() => {
+      console.log('Database connected successfully.');
+      app.listen(3000, () => {
+        console.log('Server is listening on port 3000');
+      });
+    })
+    .catch(() => {
+      console.log('Connection to database failed.');
+    });
+})();
