69dc5f6b10f20ce63148621f70dffa211abcc634
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-06 10:09:48 -0800

    [ADD] User repo with all operations done.

---
+++ src/database/repositories/UserRepo.ts
@@ -0,0 +1,35 @@
+import { Types } from 'mongoose';
+
+import UserModel from '../models/User.model';
+import IUser from '../../types/user.type';
+
+export default class UserRepo {
+  public static async create(user: IUser): Promise<IUser> {
+    const newRec = await UserModel.create(user);
+    return newRec.toObject();
+  }
+
+  public static async fetchAll(): Promise<IUser[]> {
+    return UserModel.find().lean<IUser[]>().exec();
+  }
+
+  public static async findById(id: Types.ObjectId): Promise<IUser | null> {
+    return UserModel.findById(id).lean<IUser>().exec();
+  }
+
+  public static async findByEmail(email: string): Promise<IUser | null> {
+    return UserModel.findOne({ email: email }).lean<IUser>().exec();
+  }
+
+  public static async update(user: IUser): Promise<any> {
+    const now = new Date();
+    user.updatedAt = now;
+    return UserModel.updateOne({ _id: user._id }, { $set: { ...user } })
+      .lean<IUser>()
+      .exec();
+  }
+
+  public static async remove(id: Types.ObjectId): Promise<IUser | null> {
+    return UserModel.findByIdAndRemove(id).lean<IUser>().exec();
+  }
+}
