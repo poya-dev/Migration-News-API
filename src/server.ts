ef88a2de25228e808a7ed24073684392c5d63624
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-06 09:17:44 -0800

    [ADD] Imported app file and listened for incomming request.

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,4 @@
-import express, { Request, Response } from 'express';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const app = express();
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World');
-});
+import app from './app';
 
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
