84368eca4609ac8e4cccfbcac324c7761af7a1bc
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-10 05:47:59 -0800

    [ADD] Added news router in index file to be access in app.

--- src/routes/v1/admin/index.ts
+++ src/routes/v1/admin/index.ts
@@ -2,10 +2,12 @@ import express from 'express';
 
 import userRoute from './user/user.route';
 import newsCategoryRoute from './news/newsCategory.route';
+import newsRoute from './news/news.route';
 
 const router = express.Router();
 
 router.use('/users', userRoute);
 router.use('/news-category', newsCategoryRoute);
+router.use('/news', newsRoute);
 
 export default router;
