facc7a4e7dd9f200e770ac467b0f19af1a80a7ea
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-10 05:47:26 -0800

    [ADD] Created news route with its implementation.

---
+++ src/routes/v1/admin/news/news.route.ts
@@ -0,0 +1,80 @@
+import express, { Request, Response } from 'express';
+import { Types } from 'mongoose';
+
+import NewsRepo from '../../../../database/repositories/NewsRepo';
+import ApiResponse from '../../../../utils/api-response';
+import News from '../../../../types/news.type';
+
+const router = express.Router();
+
+router.post('/', async (req: Request, res: Response) => {
+  const rec = await NewsRepo.findByTitle(req.body.title);
+  if (rec)
+    return ApiResponse.failureResponse(res, 401, 'Record already exists.');
+  const newRec = await NewsRepo.create({
+    title: req.body.title,
+    content: req.body.content,
+    imageUrl: req.body.imageUrl,
+    category: req.body.category,
+  } as News);
+  return ApiResponse.successResponse(
+    res,
+    201,
+    newRec,
+    'Record created successfully.'
+  );
+});
+
+router.get('/', async (req: Request, res: Response) => {
+  const recs = await NewsRepo.findAll();
+  return ApiResponse.successResponse(
+    res,
+    200,
+    recs,
+    'Records fetched successfully.'
+  );
+});
+
+router.get('/id/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const rec = await NewsRepo.findById(new Types.ObjectId(id));
+  if (!rec) return ApiResponse.failureResponse(res, 404, 'Record not found.');
+  return ApiResponse.successResponse(
+    res,
+    200,
+    rec,
+    'Record fetched successfully.'
+  );
+});
+
+router.put('/id/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const rec = await NewsRepo.findById(new Types.ObjectId(id));
+  if (!rec) return ApiResponse.failureResponse(res, 404, 'Record not found');
+  if (req.body.title) rec.title = req.body.title;
+  if (req.body.content) rec.content = req.body.content;
+  if (req.body.imageUrl) rec.imageUrl = req.body.imageUrl;
+  if (req.body.category) rec.category = req.body.category;
+  const updateRec = await NewsRepo.update(rec);
+  return ApiResponse.successResponse(
+    res,
+    200,
+    updateRec,
+    'Record updated successfully'
+  );
+});
+
+router.delete('/id/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const rec = await NewsRepo.findById(new Types.ObjectId(id));
+  if (!rec) return ApiResponse.failureResponse(res, 404, 'Record not found');
+  const deleteRec = await NewsRepo.remove(new Types.ObjectId(id));
+  return ApiResponse.successResponse(
+    res,
+    200,
+    deleteRec,
+    'Record deleted successfully.'
+  );
+});
+
+export default router;
