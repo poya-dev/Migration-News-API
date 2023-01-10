b8df11a4f1dba3662a1aa3c8d106f6e85645176d
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-10 06:07:53 -0800

    [ADD] Created route file for channel and implemented CRUD routes.

---
+++ src/routes/v1/admin/channel/channel.route.ts
@@ -0,0 +1,79 @@
+import express, { Request, Response } from 'express';
+import { Types } from 'mongoose';
+
+import ChannelRepo from '../../../../database/repositories/ChannelRepo';
+import ApiResponse from '../../../../utils/api-response';
+import Channel from '../../../../types/channel.type';
+
+const router = express.Router();
+
+router.post('/', async (req: Request, res: Response) => {
+  const rec = await ChannelRepo.findByName(req.body.name);
+  if (rec)
+    return ApiResponse.failureResponse(res, 401, 'Record already exists.');
+  const newRec = await ChannelRepo.create({
+    name: req.body.name,
+    iconUrl: req.body.iconUrl,
+    createdBy: req.body.user,
+    updatedBy: req.body.user,
+  } as Channel);
+  return ApiResponse.successResponse(
+    res,
+    201,
+    newRec,
+    'Record created successfully.'
+  );
+});
+
+router.get('/', async (req: Request, res: Response) => {
+  const recs = await ChannelRepo.findAll();
+  return ApiResponse.successResponse(
+    res,
+    200,
+    recs,
+    'Records fetched successfully.'
+  );
+});
+
+router.get('/id/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const rec = await ChannelRepo.findById(new Types.ObjectId(id));
+  if (!rec) return ApiResponse.failureResponse(res, 404, 'Record not found.');
+  return ApiResponse.successResponse(
+    res,
+    200,
+    rec,
+    'Record fetched successfully.'
+  );
+});
+
+router.put('/id/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const rec = await ChannelRepo.findById(new Types.ObjectId(id));
+  if (!rec) return ApiResponse.failureResponse(res, 404, 'Record not found');
+  if (req.body.name) rec.name = req.body.name;
+  if (req.body.iconUrl) rec.iconUrl = req.body.iconUrl;
+  if (req.body.user) rec.updatedBy = req.body.user;
+  const updateRec = await ChannelRepo.update(rec);
+  return ApiResponse.successResponse(
+    res,
+    200,
+    updateRec,
+    'Record updated successfully'
+  );
+});
+
+router.delete('/id/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const rec = await ChannelRepo.findById(new Types.ObjectId(id));
+  if (!rec) return ApiResponse.failureResponse(res, 404, 'Record not found');
+  const deleteRec = await ChannelRepo.remove(new Types.ObjectId(id));
+  return ApiResponse.successResponse(
+    res,
+    200,
+    deleteRec,
+    'Record deleted successfully.'
+  );
+});
+
+export default router;
