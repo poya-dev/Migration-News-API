4427c1d7e43071fa85d2b0b7a6b758d4667f30b8
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-10 03:33:56 -0800

    [ADD] Created news category repo with crud operations.

---
+++ src/database/repositories/NewsCategoryRepo.ts
@@ -0,0 +1,36 @@
+import { Types } from 'mongoose';
+
+import NewsCategoryModel from '../models/NewsCategory.model';
+import NewsCategory from '../../types/newsCategory.type';
+
+export default class NewsCategoryRepo {
+  public static async create(
+    newsCategory: NewsCategory
+  ): Promise<NewsCategory> {
+    const newRec = await NewsCategoryModel.create(newsCategory);
+    return newRec.toObject();
+  }
+
+  public static async findAll(): Promise<NewsCategory[]> {
+    return NewsCategoryModel.find().lean<NewsCategory[]>().exec();
+  }
+
+  public static async findById(): Promise<NewsCategory | null> {
+    return NewsCategoryModel.find().lean<NewsCategory | null>().exec();
+  }
+
+  public static async update(newsCategory: NewsCategory): Promise<any> {
+    const now = new Date();
+    newsCategory.updatedAt = now;
+    return NewsCategoryModel.updateOne(
+      { _id: newsCategory._id },
+      { $set: { ...newsCategory } }
+    )
+      .lean<NewsCategory>()
+      .exec();
+  }
+
+  public static async remove(id: Types.ObjectId): Promise<NewsCategory | null> {
+    return NewsCategoryModel.findByIdAndRemove(id).lean<NewsCategory>().exec();
+  }
+}
