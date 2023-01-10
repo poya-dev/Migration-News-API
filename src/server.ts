a5aa16582ce8c496d0eb2f6e6249c1c640f0ef49
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-09 22:54:53 -0800

    [IMP] Used config data instead hard code.

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,16 @@
 import mongoose from 'mongoose';
 
 import app from './app';
+import { db, port } from './config';
 
 (async () => {
   mongoose.set({ strictQuery: true });
   mongoose
-    .connect('mongodb://127.0.0.1:27017/news-app-db')
+    .connect(`mongodb://${db.host}:${db.port}/${db.name}`)
     .then(() => {
       console.log('Database connected successfully.');
-      app.listen(3000, () => {
-        console.log('Server is listening on port 3000');
+      app.listen(port, () => {
+        console.log(`Server is listening on port ${port}`);
       });
     })
     .catch(() => {
