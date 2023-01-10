ad7bd2d18df9d2ad1d38cbd40e884111491d30be
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-10 06:08:41 -0800

    [ADD] Channel router file added in index file in order to be access from app general route.

--- src/routes/v1/admin/index.ts
+++ src/routes/v1/admin/index.ts
@@ -3,11 +3,13 @@ import express from 'express';
 import userRoute from './user/user.route';
 import newsCategoryRoute from './news/newsCategory.route';
 import newsRoute from './news/news.route';
+import channelRoute from './channel/channel.route';
 
 const router = express.Router();
 
 router.use('/users', userRoute);
 router.use('/news-category', newsCategoryRoute);
 router.use('/news', newsRoute);
+router.use('/channel', channelRoute);
 
 export default router;
