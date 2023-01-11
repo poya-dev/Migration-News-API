03f1da61cdc40925123450510e8e8ed8d4ba1a96
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-11 01:57:45 -0800

    [ADD] Added sign-up and login route in index file to be used in app

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 
+import authRoute from './auth';
 import adminRoute from './admin';
 
 const router = express.Router();
 
+router.use('/auth', authRoute);
 router.use('/admin', adminRoute);
 
 export default router;
