426d51b9af635791ccf3ec2fcc00ed3b6f61c025
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-11 02:32:51 -0800

    Minor changes.

--- src/utils/auth.utils.ts
+++ src/utils/auth.utils.ts
@@ -1,23 +1,20 @@
 import jwt, { SignOptions } from 'jsonwebtoken';
 
-import {
-  testSecretKey,
-  accessTokenPrivateKey,
-  accessTokenPublicKey,
-} from '../config';
+import { testSecretKey } from '../config'; // will be removed
+// import { accessTokenPrivateKey, accessTokenPublicKey } from '../config';
 
 export const signJwt = (payload: Object, options: SignOptions = {}) => {
-  const privateKey = Buffer.from(accessTokenPrivateKey, 'base64').toString(
-    'ascii'
-  );
+  // const privateKey = Buffer.from(accessTokenPrivateKey, 'base64').toString(
+  //   'ascii'
+  // );
   return jwt.sign(payload, testSecretKey, {
     ...(options && options),
   });
 };
 
 export const verifyJwt = (token: string) => {
-  const publicKey = Buffer.from(accessTokenPublicKey, 'base64').toString(
-    'ascii'
-  );
+  // const publicKey = Buffer.from(accessTokenPublicKey, 'base64').toString(
+  //   'ascii'
+  // );
   return jwt.verify(token, testSecretKey);
 };
