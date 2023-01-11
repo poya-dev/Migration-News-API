75e5bce2d10de10ac54079e1e0dcaf737ca1e63c
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-11 00:22:46 -0800

    [ADD] Added one more method for sending token to user.

--- src/utils/api-response.ts
+++ src/utils/api-response.ts
@@ -1,6 +1,19 @@
 import { Response } from 'express';
 
 export default class ApiResponse {
+  public static sendAccessToken(
+    res: Response,
+    code: number,
+    token: string,
+    message = 'successfully'
+  ) {
+    return res.status(code).json({
+      status: 'success',
+      accessToken: token,
+      message,
+    });
+  }
+
   public static successResponse(
     res: Response,
     code: number,
