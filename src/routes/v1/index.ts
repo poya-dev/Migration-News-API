1c52e4820a680322326e9a6d8dad6829e91546d5
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-11 02:36:12 -0800

    [ADD] Added requiredLogin middleware in admin route.

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 
+import requiredLogin from '../../middlewares/requiredLogin';
 import authRoute from './auth';
 import adminRoute from './admin';
 
 const router = express.Router();
 
 router.use('/auth', authRoute);
-router.use('/admin', adminRoute);
+router.use('/admin', requiredLogin, adminRoute);
 
 export default router;
