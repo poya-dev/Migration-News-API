00f0d6f45be9243ac868abbc5d9715f206bbc86b
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-11 22:46:00 -0800

    [IMP] requiredLogin middleware improved.

--- src/middlewares/requiredLogin.ts
+++ src/middlewares/requiredLogin.ts
@@ -1,20 +1,22 @@
 import { Request, Response, NextFunction } from 'express';
+import { Types } from 'mongoose';
 
-// import UserRepo from '../database/repositories/UserRepo';
+import UserRepo from '../database/repositories/UserRepo';
 import { verifyJwt } from '../utils/auth.utils';
 import ApiResponse from '../utils/api-response';
 
 export default async (req: Request, res: Response, next: NextFunction) => {
-  if (!req.headers.authorization)
+  const { authorization } = req.headers;
+  if (!authorization || !authorization.startsWith('Bearer'))
     return ApiResponse.failureResponse(res, 401, 'Token not provided');
-  if (!req.headers.authorization.startsWith('Bearer'))
-    return ApiResponse.failureResponse(res, 401, 'Token not provided');
-  const token = req.headers.authorization.split(' ')[1];
+  const token = authorization.split(' ')[1];
   try {
     const decoded = verifyJwt(token);
-    console.log('::::decoded::::', decoded);
+    const user = await UserRepo.findById(new Types.ObjectId(decoded._id));
+    if (!user)
+      return ApiResponse.failureResponse(res, 404, 'User does not exists');
+    req.user = user;
     next();
-    // res.locals.user = await UserRepo.findById(decoded._id);
   } catch (err: any) {
     return ApiResponse.failureResponse(res, 401, 'Invalid token');
   }
