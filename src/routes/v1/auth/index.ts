8f11304c91c37e0639b0806c5b09081e77a4e363
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-13 00:26:37 -0800

    [ADD] Imported facebook-sign-in route in index file.

--- src/routes/v1/auth/index.ts
+++ src/routes/v1/auth/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 
+import facebookSignIn from './facebook-sign-in.route';
 import singUpRoute from './sign-up.route';
 import loginRoute from './login.route';
 
@@ -7,5 +8,6 @@ const router = express.Router();
 
 router.use('/sign-up', singUpRoute);
 router.use('/login', loginRoute);
+router.use('/facebook', facebookSignIn);
 
 export default router;
