55096ecd86933ca06ae402c7177ee3b80bd3e803
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-13 00:21:54 -0800

    [ADD] Added passport strategy service middleware for facebook auth.

---
+++ src/services/facebookPassport.service.ts
@@ -0,0 +1,24 @@
+import passport from 'passport';
+import FacebookTokenStrategy from 'passport-facebook-token';
+
+import { facebookClientId, facebookClientSecret } from '../config';
+import UserRepo from '../database/repositories/UserRepo';
+
+passport.use(
+  new FacebookTokenStrategy(
+    {
+      clientID: facebookClientId,
+      clientSecret: facebookClientSecret,
+      fbGraphVersion: 'v3.1',
+    },
+    async (
+      accessToken: string,
+      refreshToken: string,
+      profile: any,
+      done: any
+    ) => {
+      const user = await UserRepo.findOrCreate(profile, accessToken);
+      return done(null, user);
+    }
+  )
+);
