8c4944a2075707c12f025d9dadc6c91510304514
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-13 04:39:05 -0800

    [ADD] Google sign-in route added in index router

--- src/routes/v1/auth/index.ts
+++ src/routes/v1/auth/index.ts
@@ -1,13 +1,15 @@
 import express from 'express';
 
 import facebookSignIn from './facebook-sign-in.route';
+import googleSignIn from './google-sign-in.route';
 import singUpRoute from './sign-up.route';
 import loginRoute from './login.route';
 
 const router = express.Router();
 
+router.use('/facebook', facebookSignIn);
+router.use('/google', googleSignIn);
 router.use('/sign-up', singUpRoute);
 router.use('/login', loginRoute);
-router.use('/facebook', facebookSignIn);
 
 export default router;
