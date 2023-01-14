4d1ecbd69cd6bec0138701ba91e601f64542498e
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-14 01:15:30 -0800

    [ADD] Created language type.

---
+++ src/types/language.type.ts
@@ -0,0 +1,14 @@
+import { Types } from 'mongoose';
+
+import User from './user.type';
+
+export default interface Language {
+  _id?: Types.ObjectId;
+  name: string;
+  iconUrl: string;
+  status: string;
+  createdBy?: User['_id'];
+  updatedBy?: User['_id'];
+  createdAt?: Date;
+  updatedAt?: Date;
+}
