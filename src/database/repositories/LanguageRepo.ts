453ed98bc8ee309235c2adce0b293049c9428dd5
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-14 01:16:40 -0800

    [ADD] Lanaugae repository created also CRUD operations implemented.

---
+++ src/database/repositories/LanguageRepo.ts
@@ -0,0 +1,68 @@
+import { Types } from 'mongoose';
+
+import LanguageModel from '../models/Language.model';
+import Language from '../../types/language.type';
+
+export default class LanguageRepo {
+  public static async create(language: Language): Promise<Language> {
+    const newRec = await LanguageModel.create(language);
+    return newRec.toObject();
+  }
+
+  public static async findAll(): Promise<Language[]> {
+    return LanguageModel.find()
+      .populate({
+        path: 'createdBy',
+        select: '_id name email',
+      })
+      .populate({
+        path: 'updatedBy',
+        select: '_id name email',
+      })
+      .lean<Language[]>()
+      .exec();
+  }
+
+  public static async findById(id: Types.ObjectId): Promise<Language | null> {
+    return LanguageModel.findById(id)
+      .populate({
+        path: 'createdBy',
+        select: '_id name email',
+      })
+      .populate({
+        path: 'updatedBy',
+        select: '_id name email',
+      })
+      .lean<Language | null>()
+      .exec();
+  }
+
+  public static async findByName(name: string): Promise<Language | null> {
+    return LanguageModel.findOne({ name: name })
+      .populate({
+        path: 'createdBy',
+        select: '_id name email',
+      })
+      .populate({
+        path: 'updatedBy',
+        select: '_id name email',
+      })
+      .lean<Language | null>()
+      .exec();
+  }
+
+  public static async update(language: Language): Promise<any> {
+    const now = new Date();
+    language.updatedAt = now;
+    return LanguageModel.updateOne(
+      { _id: language._id },
+      { $set: { ...language } }
+    )
+      .lean<Language>()
+      .exec();
+  }
+
+  public static async remove(id: Types.ObjectId): Promise<Language | null> {
+    return LanguageModel.findByIdAndRemove(id).lean<Language>().exec();
+  }
+}
