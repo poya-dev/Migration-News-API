7d9feb174e167251a050eb053a37837aabfbb841
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-14 02:10:49 -0800

    [ADD] Added UpdateLastSeen middleware both for client and admin users.

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 
+import updateLastActive from '../../middlewares/updateLastActive';
 import requiredLogin from '../../middlewares/requiredLogin';
 import authRoute from './auth';
 import adminRoute from './admin';
@@ -8,7 +9,7 @@ import clientRoute from './client';
 const router = express.Router();
 
 router.use('/auth', authRoute);
-router.use('/admin', requiredLogin, adminRoute);
-router.use('/client', requiredLogin, clientRoute);
+router.use('/admin', requiredLogin, updateLastActive, adminRoute);
+router.use('/client', requiredLogin, updateLastActive, clientRoute);
 
 export default router;
