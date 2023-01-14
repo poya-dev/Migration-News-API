bd799fe2441003e7342a7219b4c9c2ed6f58e6fc
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-14 01:20:08 -0800

    [ADD] Added language router in index file.

--- src/routes/v1/admin/index.ts
+++ src/routes/v1/admin/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import userRoute from './user/user.route';
+import languageRoute from './language/language.route';
 import newsCategoryRoute from './news/newsCategory.route';
 import newsRoute from './news/news.route';
 import channelRoute from './channel/channel.route';
@@ -8,6 +9,7 @@ import channelRoute from './channel/channel.route';
 const router = express.Router();
 
 router.use('/users', userRoute);
+router.use('/language', languageRoute);
 router.use('/news-category', newsCategoryRoute);
 router.use('/news', newsRoute);
 router.use('/channel', channelRoute);
