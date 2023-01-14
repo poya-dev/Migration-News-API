2ff053369a21c87301752033a1b610104331da08
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-14 01:19:47 -0800

    [ADD] Language route implemented.

---
+++ src/routes/v1/admin/language/language.route.ts
@@ -0,0 +1,80 @@
+import express, { Request, Response } from 'express';
+import { Types } from 'mongoose';
+
+import LanguageRepo from '../../../../database/repositories/LanguageRepo';
+import ApiResponse from '../../../../utils/api-response';
+import Language from '../../../../types/language.type';
+import User from '../../../../types/user.type';
+
+const router = express.Router();
+
+router.post('/', async (req: Request, res: Response) => {
+  const rec = await LanguageRepo.findByName(req.body.name);
+  if (rec)
+    return ApiResponse.failureResponse(res, 401, 'Record already exists');
+  const newRec = await LanguageRepo.create({
+    name: req.body.name,
+    iconUrl: req.body.iconUrl,
+    createdBy: (req.user as User)._id,
+    updatedBy: (req.user as User)._id,
+  } as Language);
+  return ApiResponse.successResponse(
+    res,
+    201,
+    newRec,
+    'Record created successfully'
+  );
+});
+
+router.get('/', async (req: Request, res: Response) => {
+  const recs = await LanguageRepo.findAll();
+  return ApiResponse.successResponse(
+    res,
+    200,
+    recs,
+    'Records fetched successfully.'
+  );
+});
+
+router.get('/id/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const rec = await LanguageRepo.findById(new Types.ObjectId(id));
+  if (!rec) return ApiResponse.failureResponse(res, 404, 'Record not found');
+  return ApiResponse.successResponse(
+    res,
+    200,
+    rec,
+    'Record fetched successfully'
+  );
+});
+
+router.put('/id/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const rec = await LanguageRepo.findById(new Types.ObjectId(id));
+  if (!rec) return ApiResponse.failureResponse(res, 404, 'Record not found');
+  if (req.body.name) rec.name = req.body.name;
+  if (req.body.iconUrl) rec.iconUrl = req.body.iconUrl;
+  rec.updatedBy = (req.user as User)._id;
+  const updateRec = await LanguageRepo.update(rec);
+  return ApiResponse.successResponse(
+    res,
+    200,
+    updateRec,
+    'Record updated successfully'
+  );
+});
+
+router.delete('/id/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const rec = await LanguageRepo.findById(new Types.ObjectId(id));
+  if (!rec) return ApiResponse.failureResponse(res, 404, 'Record not found');
+  const deleteRec = await LanguageRepo.remove(new Types.ObjectId(id));
+  return ApiResponse.successResponse(
+    res,
+    200,
+    deleteRec,
+    'Record deleted successfully'
+  );
+});
+
+export default router;
