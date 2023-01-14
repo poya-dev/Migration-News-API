3ec7d56e7cd843945ccc60bc1b4282f70f0e6954
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-14 02:07:21 -0800

    [ADD] Added route for fetching all and individual news category.

---
+++ src/routes/v1/client/news/newsCategory.route.ts
@@ -0,0 +1,31 @@
+import express, { Request, Response } from 'express';
+import { Types } from 'mongoose';
+
+import NewsCategoryRepo from '../../../../database/repositories/NewsCategoryRepo';
+import ApiResponse from '../../../../utils/api-response';
+
+const router = express.Router();
+
+router.get('/', async (req: Request, res: Response) => {
+  const recs = NewsCategoryRepo.findAll();
+  return ApiResponse.successResponse(
+    res,
+    200,
+    recs,
+    'Records fetched successfully'
+  );
+});
+
+router.get('/id/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const rec = await NewsCategoryRepo.findById(new Types.ObjectId(id));
+  if (!rec) return ApiResponse.failureResponse(res, 404, 'Record not found');
+  return ApiResponse.successResponse(
+    res,
+    200,
+    rec,
+    'Record fetched successfully'
+  );
+});
+
+export default router;
