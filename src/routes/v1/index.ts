6b85273b763fdeacd7f0895f3b98bc10ea10acd1
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-14 02:08:46 -0800

    [ADD] Client route added in index router file.

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 
 import requiredLogin from '../../middlewares/requiredLogin';
-import adminRoute from './admin';
 import authRoute from './auth';
+import adminRoute from './admin';
+import clientRoute from './client';
 
 const router = express.Router();
 
 router.use('/auth', authRoute);
 router.use('/admin', requiredLogin, adminRoute);
+router.use('/client', requiredLogin, clientRoute);
 
 export default router;
