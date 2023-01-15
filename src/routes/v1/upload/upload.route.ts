db5ff957debb447b5dbe0c307b4eea32de16fa4f
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-14 23:37:32 -0800

    [ADD] Created route for uploading file returns the file uploaded path.

---
+++ src/routes/v1/upload/upload.route.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from 'express';
+
+import ApiResponse from '../../../utils/api-response';
+
+const router = express.Router();
+
+router.post('/', (req: Request, res: Response) => {
+  const imagePath = req.file?.path;
+  return ApiResponse.successResponse(
+    res,
+    201,
+    imagePath,
+    'File uploaded successfully'
+  );
+});
+
+export default router;
