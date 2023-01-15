bc98d72bf0bd1e331d63a5f2458cc85041a11368
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-14 23:38:14 -0800

    [ADD] Added endpoint for file uploading and used multer middleware.

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 
 import updateLastActive from '../../middlewares/updateLastActive';
 import requiredLogin from '../../middlewares/requiredLogin';
+import upload from '../../middlewares/fileUpload';
+import uploadRoute from './upload/upload.route';
 import authRoute from './auth';
 import adminRoute from './admin';
 import clientRoute from './client';
@@ -9,6 +11,7 @@ import clientRoute from './client';
 const router = express.Router();
 
 router.use('/auth', authRoute);
+router.use('/upload', requiredLogin, upload.single('image'), uploadRoute);
 router.use('/admin', requiredLogin, updateLastActive, adminRoute);
 router.use('/client', requiredLogin, updateLastActive, clientRoute);
 
