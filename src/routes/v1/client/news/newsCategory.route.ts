244553eca14d55b49a52e85e73381a0e69a4c960
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-16 03:06:49 -0800

    [ADD] Called method for fetching data based on language code also removed another endpoint as it is not needed.

--- src/routes/v1/client/news/newsCategory.route.ts
+++ src/routes/v1/client/news/newsCategory.route.ts
@@ -1,5 +1,4 @@
 import express, { Request, Response } from 'express';
-import { Types } from 'mongoose';
 
 import NewsCategoryRepo from '../../../../database/repositories/NewsCategoryRepo';
 import ApiResponse from '../../../../utils/api-response';
@@ -7,24 +6,13 @@ import ApiResponse from '../../../../utils/api-response';
 const router = express.Router();
 
 router.get('/', async (req: Request, res: Response) => {
-  const recs = await NewsCategoryRepo.findAll();
+  const langCode = req.query.langCode as string;
+  const recs = await NewsCategoryRepo.findByLangCode(langCode);
   return ApiResponse.successResponse(
     res,
     200,
     recs,
     'Records fetched successfully'
-  );
-});
-
-router.get('/id/:id', async (req: Request, res: Response) => {
-  const { id } = req.params;
-  const rec = await NewsCategoryRepo.findById(new Types.ObjectId(id));
-  if (!rec) return ApiResponse.failureResponse(res, 404, 'Record not found');
-  return ApiResponse.successResponse(
-    res,
-    200,
-    rec,
-    'Record fetched successfully'
   );
 });
 
