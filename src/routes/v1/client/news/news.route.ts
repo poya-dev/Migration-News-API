45d4c8ba2b4f60d2276c8627285483141bc23828
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-16 03:27:00 -0800

    [ADD] Data fetched based on langCode query.

--- src/routes/v1/client/news/news.route.ts
+++ src/routes/v1/client/news/news.route.ts
@@ -1,13 +1,20 @@
 import express, { Request, Response } from 'express';
 import { Types } from 'mongoose';
 
+import LanguageRepo from '../../../../database/repositories/LanguageRepo';
 import NewsRepo from '../../../../database/repositories/NewsRepo';
 import ApiResponse from '../../../../utils/api-response';
 
 const router = express.Router();
 
 router.get('/', async (req: Request, res: Response) => {
-  const recs = await NewsRepo.findAll();
+  const langCode = req.query.langCode as string;
+  const language = await LanguageRepo.findByCode(langCode);
+  if (!language)
+    return ApiResponse.failureResponse(res, 404, 'Language not found');
+  const recs = await NewsRepo.findNewsByLangId(
+    new Types.ObjectId(language._id)
+  );
   return ApiResponse.successResponse(
     res,
     200,
