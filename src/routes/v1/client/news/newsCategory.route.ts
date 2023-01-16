c2bfe2444c605b4bcbc961e2d23bf9ab1fc9506d
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-01-16 03:33:16 -0800

    [ADD] Added checking wheater language exist then fetched data based on language.

--- src/routes/v1/client/news/newsCategory.route.ts
+++ src/routes/v1/client/news/newsCategory.route.ts
@@ -1,13 +1,20 @@
 import express, { Request, Response } from 'express';
+import { Types } from 'mongoose';
 
 import NewsCategoryRepo from '../../../../database/repositories/NewsCategoryRepo';
+import LanguageRepo from '../../../../database/repositories/LanguageRepo';
 import ApiResponse from '../../../../utils/api-response';
 
 const router = express.Router();
 
 router.get('/', async (req: Request, res: Response) => {
   const langCode = req.query.langCode as string;
-  const recs = await NewsCategoryRepo.findByLangCode(langCode);
+  const language = await LanguageRepo.findByCode(langCode);
+  if (!language)
+    return ApiResponse.failureResponse(res, 404, 'Language not found');
+  const recs = await NewsCategoryRepo.findByLangId(
+    new Types.ObjectId(language._id)
+  );
   return ApiResponse.successResponse(
     res,
     200,
