9b978c0e6bb8d4edcd5f8e292d11f319f0eb91aa
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-02-02 02:16:11 -0800

    [ADD] Added type for consultation

---
+++ src/types/consultation.type.ts
@@ -0,0 +1,14 @@
+import { Types } from 'mongoose';
+
+import User from './user.type';
+
+export default interface Consultation {
+  _id?: Types.ObjectId;
+  name: string;
+  address: string;
+  phone: string;
+  response?: object;
+  createdBy?: User['_id'];
+  createdAt?: Date;
+  updatedAt?: Date;
+}
