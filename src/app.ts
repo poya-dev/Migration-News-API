629085ca1165639caa0e184e2cf6d63d42715c1b
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-02-02 07:23:52 -0800

    [IMP] Minor changes which is needed in order to setup socket.io

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,32 @@
+import express from 'express';
 import bodyParser from 'body-parser';
 import passport from 'passport';
-import express from 'express';
+import { createServer } from 'http';
 import cors from 'cors';
+import path from 'path';
 
 import './services/facebookPassport.service';
 
-// NEWS API V1 route
+// API V1 route
 import RouteV1 from './routes/v1';
 
 const app = express();
 
+const whitelist = ['http://localhost:3001', '*'];
+
+const corsOptions = {
+  credentials: true,
+  optionsSuccessStatus: 200,
+  origin: (origin: any, callback: any) => {
+    if (!origin) {
+      return callback(null, true);
+    }
+    if (whitelist.includes(origin)) return callback(null, true);
+    callback(new Error('Not allowed by CORS'));
+  },
+};
+
+app.use('/uploads/images', express.static(path.join(__dirname, '../uploads')));
 app.use(passport.initialize());
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(
@@ -19,24 +36,11 @@ app.use(
     parameterLimit: 50000,
   })
 );
-
-const whitelist = ['http://localhost:3001', '*'];
-
-const corsOptions = {
-  credentials: true,
-  optionsSuccessStatus: 200,
-  origin: (origin: any, callback: any) => {
-    if (!origin) {
-      //for bypassing postman req with  no origin
-      return callback(null, true);
-    }
-    if (whitelist.includes(origin)) return callback(null, true);
-    callback(new Error('Not allowed by CORS'));
-  },
-};
-
 app.use(cors(corsOptions));
 
+// ROOT API ENDPOINT
 app.use('/api/v1', RouteV1);
 
-export default app;
+const httpServer = createServer(app);
+
+export default httpServer;
