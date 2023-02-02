6a7c178dd62a7ceccd738335acbaa9279da791ca
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-02-02 02:23:12 -0800

    [ADD] Added route for consultation and used consultation router

--- src/routes/v1/client/index.ts
+++ src/routes/v1/client/index.ts
@@ -2,12 +2,14 @@ import express from 'express';
 
 import newsCategoryRoute from './news/newsCategory.route';
 import userActivityRoute from './activity/userActivity.route';
+import consultationRoute from './consultation/consultation.route';
 import newsRoute from './news/news.route';
 
 const router = express.Router();
 
 router.use('/news-category', newsCategoryRoute);
 router.use('/user-activity', userActivityRoute);
+router.use('/consultation', consultationRoute);
 router.use('/news', newsRoute);
 
 export default router;
