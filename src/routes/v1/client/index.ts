43dab2af9d2d7207bad8de28145448e60b741db5
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-02-01 22:16:19 -0800

    [ADD] File name changed also imported userActivity route in index router file

--- src/routes/v1/client/index.ts
+++ src/routes/v1/client/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 
 import newsCategoryRoute from './news/newsCategory.route';
+import userActivityRoute from './activity/userActivity.route';
 import newsRoute from './news/news.route';
 
 const router = express.Router();
 
 router.use('/news-category', newsCategoryRoute);
+router.use('/user-activity', userActivityRoute);
 router.use('/news', newsRoute);
 
 export default router;
