b66889b1518f5bc76d84117b34f8b12fde96bfd3
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-02-03 22:34:34 -0800

    consulation repo renamed

---
+++ src/database/repositories/ConsultingRepo.ts
@@ -0,0 +1,62 @@
+import { Types } from 'mongoose';
+
+import ConsultingModel from '../models/Consulting.model';
+import Consulting from '../../types/consulting.type';
+
+export default class ConsultationRepo {
+  public static async create(request: Consulting): Promise<Consulting> {
+    const newRec = await ConsultingModel.create(request);
+    return newRec.toObject();
+  }
+
+  public static async findAll(): Promise<Consulting[]> {
+    return ConsultingModel.find()
+      .populate('response')
+      .populate({
+        path: 'createdBy',
+        select: '_id name email',
+      })
+      .lean<Consulting[]>()
+      .exec();
+  }
+
+  public static async findById(id: Types.ObjectId): Promise<Consulting | null> {
+    return ConsultingModel.findById(id)
+      .populate('response')
+      .populate({
+        path: 'createdBy',
+        select: '_id name email',
+      })
+      .lean<Consulting | null>()
+      .exec();
+  }
+
+  public static async findByUserId(
+    id: Types.ObjectId
+  ): Promise<Consulting | null> {
+    return ConsultingModel.find({ user: id })
+      .populate('response')
+      .populate({
+        path: 'createdBy',
+        select: '_id name email',
+      })
+      .lean<Consulting | null>()
+      .exec();
+  }
+
+  public static async addResponse(
+    id: Types.ObjectId,
+    responseMessage: string
+  ): Promise<any> {
+    const now = new Date();
+    return ConsultingModel.findByIdAndUpdate(id, {
+      $set: { 'response.message': responseMessage, 'response.createdAt': now },
+    })
+      .lean<Consulting>()
+      .exec();
+  }
+
+  public static async remove(id: Types.ObjectId): Promise<Consulting | null> {
+    return ConsultingModel.findByIdAndRemove(id).lean<Consulting>().exec();
+  }
+}
