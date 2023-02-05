38d1d08d068597e9f8b842d2e82d482b776d39df
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-02-05 01:47:59 -0800

    [IMP] Minor improvement

--- src/routes/v1/admin/news/news.route.ts
+++ src/routes/v1/admin/news/news.route.ts
@@ -10,6 +10,8 @@ import socket from '../../../../socket';
 const router = express.Router();
 
 router.post('/', async (req: Request, res: Response) => {
+  const { name, email } = req.user as User;
+  const user = { name, email };
   const rec = await NewsRepo.findByTitle(req.body.title);
   if (rec)
     return ApiResponse.failureResponse(res, 401, 'Record already exists.');
@@ -20,8 +22,8 @@ router.post('/', async (req: Request, res: Response) => {
     language: req.body.language,
     category: req.body.category,
     channel: req.body.channel,
-    createdBy: (req.user as User)._id,
-    updatedBy: (req.user as User)._id,
+    createdBy: user,
+    updatedBy: user,
   } as News);
   return ApiResponse.successResponse(res, 201, newRec);
 });
@@ -40,6 +42,7 @@ router.get('/id/:id', async (req: Request, res: Response) => {
 
 router.put('/id/:id/', async (req: Request, res: Response) => {
   const { id } = req.params;
+  const { name, email } = req.user as User;
   const rec = await NewsRepo.findById(new Types.ObjectId(id));
   if (!rec) return ApiResponse.failureResponse(res, 404, 'Record not found.');
   if (req.body.title) rec.title = req.body.title;
@@ -48,7 +51,7 @@ router.put('/id/:id/', async (req: Request, res: Response) => {
   if (req.body.language) rec.language = req.body.language;
   if (req.body.category) rec.category = req.body.category;
   if (req.body.channel) rec.channel = req.body.channel;
-  rec.updatedBy = (req.user as User)._id;
+  rec.updatedBy = { name: name, email: email };
   await NewsRepo.update(rec);
   return ApiResponse.successResponse(res, 200, rec);
 });
@@ -88,6 +91,7 @@ router.put('/id/:id/deactivated', async (req: Request, res: Response) => {
 
 router.put('/id/:id/deactivated', async (req: Request, res: Response) => {
   const { id } = req.params;
+  const { name, email } = req.user as User;
   const rec = await NewsRepo.findById(new Types.ObjectId(id));
   if (!rec) return ApiResponse.failureResponse(res, 404, 'Record not found');
   if (req.body.title) rec.title = req.body.title;
@@ -96,7 +100,7 @@ router.put('/id/:id/deactivated', async (req: Request, res: Response) => {
   if (req.body.language) rec.language = req.body.language;
   if (req.body.category) rec.category = req.body.category;
   if (req.body.channel) rec.channel = req.body.channel;
-  rec.updatedBy = (req.user as User)._id;
+  rec.updatedBy = { name: name, email: email };
   const updateRec = await NewsRepo.update(rec);
   return ApiResponse.successResponse(res, 200, updateRec);
 });
