7d66a090e76e1a0039ac1c7eaec1879a230473b9
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-02-07 02:45:37 -0800

    Added profile endpoint and its handler

--- src/routes/v1/client/index.ts
+++ src/routes/v1/client/index.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import newsCategoryRoute from './news/newsCategory.route';
 import bookmarkRoute from './bookmark/bookmark.route';
 import consultingRoute from './consulting/consulting.route';
+import profileRoute from './profile/profile.route';
 import newsRoute from './news/news.route';
 
 const router = express.Router();
@@ -10,6 +11,7 @@ const router = express.Router();
 router.use('/news-category', newsCategoryRoute);
 router.use('/bookmark', bookmarkRoute);
 router.use('/consulting', consultingRoute);
+router.use('/profile', profileRoute);
 router.use('/news', newsRoute);
 
 export default router;
