58dd30be674e5ad6cc113ff41e33439cc4eacaa2
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-02-08 21:17:24 -0800

    [IMP] Prapered chart data of monthly online users

--- src/routes/v1/admin/dashboard/dashboard.route.ts
+++ src/routes/v1/admin/dashboard/dashboard.route.ts
@@ -1,16 +1,34 @@
 import express, { Request, Response } from 'express';
 
 import DashboardRepo from '../../../../database/repositories/DashboardRepo';
+import numToMonthName from '../../../../utils/number-to-month-name';
 import ApiResponse from '../../../../utils/api-response';
 
 const router = express.Router();
+
+type datasetType = { label: string; data: number[] };
+type userActivityType = { labels: string[]; datasets: datasetType };
 
 router.get('/', async (req: Request, res: Response) => {
   const languageCount = await DashboardRepo.countLanguage();
   const channelCount = await DashboardRepo.countChannel();
   const categoryCount = await DashboardRepo.countCategory();
+  const consultingCount = await DashboardRepo.countConsulting();
   const newsCount = await DashboardRepo.countNews();
   const userCount = await DashboardRepo.countUser();
+  const chartArray = await DashboardRepo.recentUsers();
+
+  let userActivity: userActivityType = {
+    labels: [],
+    datasets: { label: 'Mobile apps', data: [] },
+  };
+
+  if (chartArray.length > 0)
+    chartArray.forEach((activeUser) => {
+      userActivity.labels.push(numToMonthName(activeUser._id));
+      userActivity.datasets.data.push(activeUser.count);
+    });
+
   const dashboardInfo = {
     language: {
       label: 'Languages',
@@ -24,6 +42,10 @@ router.get('/', async (req: Request, res: Response) => {
       label: 'Categories',
       count: categoryCount,
     },
+    consulting: {
+      label: 'Consulting request',
+      count: consultingCount,
+    },
     news: {
       label: 'News',
       count: newsCount,
@@ -32,6 +54,7 @@ router.get('/', async (req: Request, res: Response) => {
       label: 'All Users',
       count: userCount,
     },
+    userChart: userActivity,
   };
   setTimeout(() => {
     ApiResponse.successResponse(res, 200, dashboardInfo);
