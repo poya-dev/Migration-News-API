b502d52afa15add28396f6b149c462095f60b37d
Hashem-Poya <poya.kpu.2020@gmail.com>
2023-02-13 04:08:47 -0800

    [IMP] Code Improvement

--- src/routes/v1/client/news/newsCategory.route.ts
+++ src/routes/v1/client/news/newsCategory.route.ts
@@ -11,13 +11,16 @@ type QueryParam = { lang?: string };
 
 router.get('/', async (req: Request, res: Response) => {
   const { lang = 'fa' } = req.query as QueryParam;
+  const allCategories: any = [
+    { _id: new Types.ObjectId(), name: lang === 'fa' ? 'همه' : 'توله' },
+  ];
   const language = await LanguageRepo.findByCode(lang);
   if (!language)
     return ApiResponse.failureResponse(res, 404, 'Language not found');
   const recs = await NewsCategoryRepo.findByLanguageId(
     new Types.ObjectId(language._id)
   );
-  return ApiResponse.successResponse(res, 200, recs);
+  return ApiResponse.successResponse(res, 200, [...allCategories, ...recs]);
 });
 
 export default router;
